e81aa9e33d3a628ded7538284297419fce2a55fa
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-22 19:51:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,11 +8,11 @@ class MP3Importer
     @@all << path
   end 
   
-  # def import 
-  #  files.each do |file|
-  #    Song.new_by_filename(file)
-  #  end 
-  # end 
+   def import 
+    files.each do |file|
+      Song.new_by_filename(file)
+    end 
+   end 
   
   def files
     Dir.entries(self.path).select {|file| file.end_with?(".mp3")}
